15692150271ad405eb76885fd3d819ccc2e0fcc2
michel <mportier@gmail.com>
2021-03-01 11:20:04 +0100

    Finished CDI and mockito demo

--- jeeLesDemo21/src/test/java/StarWarsTest.java
+++ jeeLesDemo21/src/test/java/StarWarsTest.java
@@ -1,14 +1,18 @@
+import oose.dea.dao.IJediDAO;
 import oose.dea.dao.JediDAO;
 import oose.dea.domain.Jedi;
 import oose.dea.service.StarWars;
 import oose.dea.service.dto.JediDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.ws.rs.core.Response;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StarWarsTest {
 
@@ -32,14 +36,24 @@ public class StarWarsTest {
     }
 
     @Test
-    public void getJediTest(){
+    public void getJediTestRegular(){
+        // Arrange
         int statuscodeExpected = 200;
         int id = 41;
         String name = "Thijmen";
+        Jedi jedi = new Jedi(id);
+        jedi.setName("Thijmen");
 
+        IJediDAO jediDAOMock = mock(IJediDAO.class);
+        when(jediDAOMock.getJedi(id)).thenReturn(jedi);
+        starWars.setJediDAO(jediDAOMock);
+
+
+        // Act
         Response response = starWars.getJedi(id);
         JediDTO jediDTO = (JediDTO) response.getEntity();
 
+        // Assert
         assertEquals(statuscodeExpected, response.getStatus());
         assertEquals(id,jediDTO.customerId);
         assertEquals(name,jediDTO.name);
@@ -47,19 +61,22 @@ public class StarWarsTest {
 
     }
 
+
     @Test
     public void getJediNotFoundTest(){
+        // Arrange
         int statuscodeExpected = 404;
-        int id = 50;
+        int id = 41;
 
+        IJediDAO jediDAOMock = mock(IJediDAO.class);
+        when(jediDAOMock.getJedi(id)).thenReturn(null);
+        starWars.setJediDAO(jediDAOMock);
+
+        // Act
         Response response = starWars.getJedi(id);
-        String responseString = (String) response.getEntity();
 
+        // Assert
         assertEquals(statuscodeExpected, response.getStatus());
-        assertEquals("niet gevonden",responseString);
-
-
-
     }
 
 
